086de2652f518f2b346e746862ad1161bcec589e
Janki256 <pada0008@algonquinlive.com>
2016-12-21 00:41:43 -0500

    Added test cases for network call api

--- MobileLiveDemoApp/MobileLiveDemoAppTests/MobileLiveDemoAppTests.m
+++ MobileLiveDemoApp/MobileLiveDemoAppTests/MobileLiveDemoAppTests.m
@@ -7,6 +7,7 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "AFNetworking.h"
 
 @interface MobileLiveDemoAppTests : XCTestCase
 
@@ -34,6 +35,178 @@
     [self measureBlock:^{
         // Put the code you want to measure the time of here.
     }];
+}
+
+- (void)testUserLogin
+{
+    XCTestExpectation *expectation = [self expectationWithDescription:@"user login request"];
+    
+    AFHTTPSessionManager *manager = [[AFHTTPSessionManager alloc] initWithBaseURL:[NSURL URLWithString:@"http://mobilelive.getsandbox.com"]];
+    
+    manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    [manager.requestSerializer setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    
+    NSDictionary *params = @{@"username": @"User1",
+                             @"password": @"User1"
+                             };
+    
+    [manager POST:@"http://mobilelive.getsandbox.com/users/login" parameters:params progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        
+        //Check for status code
+        if ([task.response isKindOfClass:[NSHTTPURLResponse class]]) {
+            
+            NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)task.response;
+            NSInteger statusCode = httpResponse.statusCode;
+            //HTTP statuscode must be 200
+            XCTAssertEqual(statusCode, 200, @"status code was not 200; was %ld", (long)statusCode);
+            
+            NSString *coockie = [[httpResponse allHeaderFields] valueForKey:@"Set-Cookie"];
+            [[NSUserDefaults standardUserDefaults] setObject:coockie forKey:@"Set-Cookie"];
+            [[NSUserDefaults standardUserDefaults] synchronize];
+        }
+        
+        //responseObject is nill
+        XCTAssert(responseObject, @"responseObject nil");
+        
+        [expectation fulfill];
+        
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        
+        //Error object is nill
+        XCTAssertNil(error, @"login error %@", error);
+    }];
+    
+    //Request should complete in 60 secs
+    [self waitForExpectationsWithTimeout:60.0 handler:nil];
+}
+
+- (void)testRegisterUser
+{
+    XCTestExpectation *expectation = [self expectationWithDescription:@"user resgistration request"];
+    
+    AFHTTPSessionManager *manager = [[AFHTTPSessionManager alloc] initWithBaseURL:[NSURL URLWithString:@"http://mobilelive.getsandbox.com"]];
+    
+    
+    manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    [manager.requestSerializer setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    
+    NSDictionary *params = @{@"username": @"testUser",
+                             @"password": @"12345",
+                             @"firstname": @"Test Fname",
+                             @"lastname": @"Test Lname",
+                             @"phone": @"111-212-1223"
+                             };
+    
+    [manager POST:@"http://mobilelive.getsandbox.com/users" parameters:params progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        
+        //Check for status code
+        if ([task.response isKindOfClass:[NSHTTPURLResponse class]]) {
+            
+            NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)task.response;
+            NSInteger statusCode = httpResponse.statusCode;
+            //HTTP statuscode must be 200
+            XCTAssertEqual(statusCode, 200, @"status code was not 200; was %ld", (long)statusCode);
+        }
+        
+        //responseObject is nill
+        XCTAssert(responseObject, @"responseObject nil");
+        
+        [expectation fulfill];
+        
+        
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        
+        //Error object is nill
+        XCTAssertNil(error, @"login error %@", error);
+    }];
+    
+    //Request should complete in 60 secs
+    [self waitForExpectationsWithTimeout:60.0 handler:nil];
+
+}
+
+- (void)testUploadPhoto
+{
+    XCTestExpectation *expectation = [self expectationWithDescription:@"asynchronous user login request"];
+    
+    AFHTTPSessionManager *manager = [[AFHTTPSessionManager alloc] initWithBaseURL:[NSURL URLWithString:@"http://mobilelive.getsandbox.com"]];
+    
+    manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    [manager.requestSerializer setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+
+    //Session-id nil
+    XCTAssert([[NSUserDefaults standardUserDefaults] objectForKey:@"Set-Cookie"], @"session-id nil");
+
+    [manager.requestSerializer setValue:[[NSUserDefaults standardUserDefaults] objectForKey:@"Set-Cookie"] forHTTPHeaderField:@"cookie"];
+    
+    UIImage *photo = [UIImage imageNamed:@"test.jpg"];
+    NSString *strImage = [UIImagePNGRepresentation(photo) base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
+    NSDictionary *params = @{@"name": @"testPhoto",
+                             @"data": strImage
+                             };
+    
+    [manager POST:@"http://mobilelive.getsandbox.com/v2/photos" parameters:params progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        
+        //Check for status code
+        if ([task.response isKindOfClass:[NSHTTPURLResponse class]]) {
+            
+            NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)task.response;
+            NSInteger statusCode = httpResponse.statusCode;
+            //HTTP statuscode must be 200
+            XCTAssertEqual(statusCode, 200, @"status code was not 200; was %ld", (long)statusCode);
+        }
+        //responseObject is nill
+        XCTAssert(responseObject, @"responseObject nil");
+        
+        [expectation fulfill];
+        
+        
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        
+        //Error object is nill
+        XCTAssertNil(error, @"login error %@", error);
+    }];
+    
+    [self waitForExpectationsWithTimeout:60.0 handler:nil];
+}
+
+- (void)testGetListOfPhotos
+{
+    XCTestExpectation *expectation = [self expectationWithDescription:@"asynchronous user login request"];
+    
+    AFHTTPSessionManager *manager = [[AFHTTPSessionManager alloc] initWithBaseURL:[NSURL URLWithString:@"http://mobilelive.getsandbox.com"]];
+    
+    manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    [manager.requestSerializer setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    
+    //Session-id nil
+    XCTAssert([[NSUserDefaults standardUserDefaults] objectForKey:@"Set-Cookie"], @"session-id nil");
+    
+    [manager.requestSerializer setValue:[[NSUserDefaults standardUserDefaults] objectForKey:@"Set-Cookie"] forHTTPHeaderField:@"cookie"];
+    
+    [manager GET:@"http://mobilelive.getsandbox.com/v2/photos" parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        
+        //Check for status code
+        if ([task.response isKindOfClass:[NSHTTPURLResponse class]]) {
+            
+            NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)task.response;
+            NSInteger statusCode = httpResponse.statusCode;
+            //HTTP statuscode must be 200
+            XCTAssertEqual(statusCode, 200, @"status code was not 200; was %ld", (long)statusCode);
+        }
+        //responseObject is nill
+        XCTAssert(responseObject, @"responseObject nil");
+        
+        [expectation fulfill];
+        
+        
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        
+        //Error object is nill
+        XCTAssertNil(error, @"login error %@", error);
+    }];
+    
+    [self waitForExpectationsWithTimeout:60.0 handler:nil];
 }
 
 @end
